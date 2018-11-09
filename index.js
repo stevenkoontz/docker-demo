491e3dbbd2d87ce5bad4f69b8a584a27fdc2b21a
stevenkoontz <31934289+stevenkoontz@users.noreply.github.com>
2018-11-09 18:58:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello <b>Bob</b>! I told you I could do it!');
+  res.send('Hello Bob! I told you I could do it!');
 });
 
 var server = app.listen(3000, function () {
